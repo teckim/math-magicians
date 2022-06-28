7b80028b47479a9ddc7171ff0b843e4da2254dec
BHDHakim <hakim.bhd@gmail.com>
2022-06-28 17:36:12 +0300

    Add Calculator component UI

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// assets
+import './assets/styles/reset.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
