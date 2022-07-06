3743878ae3710f6c0c595bb02b1fa80b546d4a30
BHDHakim <hakim.bhd@gmail.com>
2022-07-06 17:00:59 +0300

    Fix linter errors

--- src/__tests__/calculator.test.js
+++ src/__tests__/calculator.test.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 import Calculator from '../components/Calculator';
-import userEvent from '@testing-library/user-event'
-import {render, screen} from '@testing-library/react'
-describe('testing math magician',()=>{
-    it('renders correctly', () => {
-        const tree = TestRenderer
-          .create(<Calculator/>)
-          .toJSON();
-        expect(tree).toMatchSnapshot();
-      });
 
-      it('Testing calc rendering using test library',()=>{
-        const{getByTestId} = render(<Calculator/>);
-        const Input = getByTestId('testingInput');
-        expect(Input).toBeTruthy();
-      })
+describe('testing math magician', () => {
+  it('renders correctly', () => {
+    const tree = TestRenderer
+      .create(<Calculator />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-      test('click', () => {
-        render(<Calculator/>)
-        userEvent.click(screen.getByText('2'))
-        userEvent.click(screen.getByText('+'))
-        userEvent.click(screen.getByText('3'))
-        userEvent.click(screen.getByText('='))
-        const value = screen.getByTestId('testingInput').value
-        expect(value).toBe('5')
-      })
-})
+  it('Testing calc rendering using test library', () => {
+    const { getByTestId } = render(<Calculator />);
+    const Input = getByTestId('testingInput');
+    expect(Input).toBeTruthy();
+  });
+
+  test('click', () => {
+    render(<Calculator />);
+    userEvent.click(screen.getByText('2'));
+    userEvent.click(screen.getByText('+'));
+    userEvent.click(screen.getByText('3'));
+    userEvent.click(screen.getByText('='));
+    const { value } = screen.getByTestId('testingInput');
+    expect(value).toBe('5');
+  });
+});
