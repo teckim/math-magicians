a42cdb574eb2d1399a674fe1f34d7786d05ba98b
BHDHakim <hakim.bhd@gmail.com>
2022-07-07 20:30:00 +0300

    Add more tests

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -6,12 +6,42 @@ describe('calculate unit', () => {
     next: 0,
   };
 
-  result = calculate(result, '1');
-  result = calculate(result, '+');
-  result = calculate(result, '3');
-  result = calculate(result, '=');
-
   test('1 + 3 should be equale to 4', () => {
+    result = calculate(result, '1');
+    result = calculate(result, '+');
+    result = calculate(result, '3');
+    result = calculate(result, '=');
+
+    expect(result.total).toBe('4');
+  });
+
+  test('5 - 3 should be equale to 2', () => {
+    result = calculate(result, 'AC');
+    result = calculate(result, '5');
+    result = calculate(result, '-');
+    result = calculate(result, '3');
+    result = calculate(result, '=');
+
+    expect(result.total).toBe('2');
+  });
+
+  test('3 x 2 should be equale to 6', () => {
+    result = calculate(result, 'AC');
+    result = calculate(result, '3');
+    result = calculate(result, 'x');
+    result = calculate(result, '2');
+    result = calculate(result, '=');
+
+    expect(result.total).toBe('6');
+  });
+
+  test('8 / 2 should be equale to 4', () => {
+    result = calculate(result, 'AC');
+    result = calculate(result, '8');
+    result = calculate(result, '÷');
+    result = calculate(result, '2');
+    result = calculate(result, '=');
+
     expect(result.total).toBe('4');
   });
 });
--- src/__tests__/calculator.test.js
+++ src/__tests__/calculator.test.js
@@ -4,27 +4,64 @@ import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
 import Calculator from '../components/Calculator';
 
-describe('testing math magician', () => {
-  it('renders correctly', () => {
-    const tree = TestRenderer
-      .create(<Calculator />)
-      .toJSON();
+describe('calculator component', () => {
+  it('should render correctly', () => {
+    const tree = TestRenderer.create(<Calculator />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it('Testing calc rendering using test library', () => {
-    const { getByTestId } = render(<Calculator />);
-    const Input = getByTestId('testingInput');
-    expect(Input).toBeTruthy();
-  });
-
-  test('click', () => {
+  test('2 + 3 should be equale to 5', () => {
     render(<Calculator />);
+
     userEvent.click(screen.getByText('2'));
     userEvent.click(screen.getByText('+'));
     userEvent.click(screen.getByText('3'));
     userEvent.click(screen.getByText('='));
-    const { value } = screen.getByTestId('testingInput');
+
+    const { value } = screen.getByTestId('calculator-input');
+
     expect(value).toBe('5');
+  });
+
+  test('5 - 3 should be equale to 2', () => {
+    render(<Calculator />);
+
+    userEvent.click(screen.getByText('AC'));
+    userEvent.click(screen.getByText('5'));
+    userEvent.click(screen.getByText('-'));
+    userEvent.click(screen.getByText('3'));
+    userEvent.click(screen.getByText('='));
+
+    const { value } = screen.getByTestId('calculator-input');
+
+    expect(value).toBe('2');
+  });
+
+  test('3 x 2 should be equale to 6', () => {
+    render(<Calculator />);
+
+    userEvent.click(screen.getByText('AC'));
+    userEvent.click(screen.getByText('3'));
+    userEvent.click(screen.getByText('x'));
+    userEvent.click(screen.getByText('2'));
+    userEvent.click(screen.getByText('='));
+
+    const { value } = screen.getByTestId('calculator-input');
+
+    expect(value).toBe('6');
+  });
+
+  test('8 ÷ 2 should be equale to 4', () => {
+    render(<Calculator />);
+
+    userEvent.click(screen.getByText('AC'));
+    userEvent.click(screen.getByText('8'));
+    userEvent.click(screen.getByText('÷'));
+    userEvent.click(screen.getByText('2'));
+    userEvent.click(screen.getByText('='));
+
+    const { value } = screen.getByTestId('calculator-input');
+
+    expect(value).toBe('4');
   });
 });
